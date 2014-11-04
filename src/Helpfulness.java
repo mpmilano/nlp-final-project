446239be36d9181b5aebde69e24eee13f43796f9
Matthew Milano <milano@cs.cornell.edu>
2014-11-03 20:47:12 -0500

    grr nullptr

--- src/Helpfulness.java
+++ src/Helpfulness.java
@@ -9,6 +9,7 @@ public class Helpfulness {
 	
 	private static HashMap< Long,  Helpfulness> lookup = new HashMap<Long, Helpfulness>();
 	public static Helpfulness build(int votes, int total){
+		if (lookup == null) lookup = new HashMap<Long, Helpfulness>(); 
 		long p = (((long) votes) << 32) + total;
 		if (lookup.containsKey(p)) return lookup.get(p);
 		Helpfulness ret = new Helpfulness(votes,total); 
