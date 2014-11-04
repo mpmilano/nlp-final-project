c15c464f40bd5c9d592b5dde8f8d9d0bf00828bd
Matthew Milano <milano@cs.cornell.edu>
2014-11-03 21:08:23 -0500

    grr split

--- src/ReviewParser.java
+++ src/ReviewParser.java
@@ -26,7 +26,7 @@ class ReviewParser {
 	
 	private String overflow = "";
 	private String rangeFromOverflow(String prefix, String done){
-		if (overflow.contains(prefix)){
+		if (overflow.contains(prefix) && !overflow.endsWith(prefix)){
 			String[] tmp = overflow.split(prefix)[1].split(done);
 			if (tmp.length > 1) overflow = done + tmp[1];
 			return prefix + tmp[0];
