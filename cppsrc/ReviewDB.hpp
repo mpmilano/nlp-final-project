e240428178fd80e47da1393658318dc9c38a6a84
Matthew Milano <milano@cs.cornell.edu>
2014-12-02 19:41:01 -0500

    database

---
+++ cppsrc/ReviewDB.hpp
@@ -0,0 +1,106 @@
+#pragma once
+#include "ReviewParser.hpp"
+#include <SQLAPI.h>
+#include <future>
+
+class ReviewDB{
+
+public:
+
+	typedef ReviewParser<std::ifstream>::sets sets;
+
+	void writeToDB(const sets &s){
+		auto task1 = [&s](){ //reviewers 
+			SAConnection con;
+			con.Connect("AmazonReviews","research","researchVM",SA_MySQL_Client);
+			
+			SACommand cmd(&con);
+			cmd.setCommandText("insert into Reviewer (profileName, userID, id) values (:1, :2, :3)");
+			for (const auto &rr : s.rrs){
+				cmd << lock(rr)->profileName.c_str();
+				cmd << lock(rr)->userID.c_str();
+				cmd << lock(rr)->id.asInt();
+				try{
+					cmd.Execute();
+				}
+				catch (SAException e){
+					std::string errtxt = e.ErrText().GetMultiByteChars();
+					if (errtxt.substr(0,15) != "Duplicate entry"){
+						sleep(5);
+						std::cerr << "Previously-unseen error!" << std::endl;
+						std::cerr << errtxt << std::endl;
+						sleep(5);
+						throw e;
+					}
+				}
+			}
+		};
+
+		auto task2 = [&s](){ //Products
+			SAConnection con;
+			con.Connect("AmazonReviews","research","researchVM",SA_MySQL_Client);
+			
+			SACommand cmd(&con);
+			cmd.setCommandText("insert into Product (ProductID, Title, Price, ProductType, ProductRef) values (:1, :2, :3, :4, :5)");
+			for (const auto &p : s.ps){
+				cmd << lock(p)->productID.c_str() << lock(p)->title.c_str() 
+					<< lock(p)->price << lock(p)->productType.c_str() << lock(p)->id.asInt();
+				try{
+					cmd.Execute();
+				}
+				catch (SAException e){
+					std::string errtxt = e.ErrText().GetMultiByteChars();
+					if (errtxt.substr(0,15) != "Duplicate entry"){
+						sleep(5);
+						std::cerr << "Previously-unseen error!" << std::endl;
+						std::cerr << errtxt << std::endl;
+						sleep(5);
+						throw e;
+					}
+				}
+			}
+		};
+
+		auto task3 = [&s](){ //reviews
+			SAConnection con;
+			con.Connect("AmazonReviews","research","researchVM",SA_MySQL_Client);
+			SACommand cmd(&con);
+			cmd.setCommandText("insert into Review (Summary, Score, Time, Reviewer, HelpYes, HelpTotal, Product, Text, Id) values (:1, :2, :3, :4, :5, :6, :7, :8, :9)");
+			for (const auto &r : s.rs){
+				long unsigned int ltime = r->time;
+				long unsigned int lhf = r->help.first;
+				long unsigned int lhs = r->help.second;
+				cmd << r->summary.get().c_str() << r->score << ltime
+					<< r->reviewer->id.asInt() << lhf << lhs
+					<< r->product->id.asInt() << r->text.get().c_str() << r->id.asInt();
+				try{
+					cmd.Execute();
+				}
+				catch (SAException e){
+					std::string errtxt = e.ErrText().GetMultiByteChars();
+					if (errtxt.substr(0,15) != "Duplicate entry"){
+						sleep(5);
+						std::cerr << "Previously-unseen error!" << std::endl;
+						std::cerr << errtxt << std::endl;
+						sleep(5);
+						throw e;
+					}
+				}
+			}
+		};
+
+		auto fut1 = std::async(std::launch::async, task1);
+		auto fut2 = std::async(std::launch::async, task2);
+		auto fut3 = std::async(std::launch::async, task3);
+		fut1.wait();
+		fut2.wait();
+		fut3.wait();
+		
+	}
+
+	void readFromDB(Reviewer::builder &, Product::builder &, Review::builder &, sets &){
+		
+	}
+
+
+};
